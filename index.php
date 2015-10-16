5226a6cdcfda3c9020edc26a71daff0eea04b7bb
brzezinavaclav <brzezina.vaclav@outlook.cz>
2015-10-16 17:33:48 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Creacore</title>
+	<link href="img/favicon.ico" rel="shortcut icon">
+    <!-- Bootstrap -->
+    <link href="css/style.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:500' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="top">
+<nav class="navbar navbar-default navbar-fixed-top" id="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#top"><img src="img/logo.png" height="22px" style="float:left"> Creacore</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#about">Co děláme</a></li>
+                <li><a href="#team">O nás</a></li>
+                <li><a href="#work">Reference</a></li>
+                <li><a href="#contact">Kontakt</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+<section id="slider">
+    <ul class="rslides">
+        <li style="background-image: url('img/bg-6.1.jpg'); background-position:center; background-size: cover"><div class="caption"><h1>Jsme milovníci promyšleného <font color="#00ACAC">designu</font></h1></div></li>
+        <li style="background: url('img/desk.jpg'); background-position:center; background-size: cover"><div class="caption"><h1>Používáme nejnovější webové <font color="#00ACAC">standardy</font></h1></div></li>
+        <li style="background: url('img/bg-8.1.jpg'); background-position:center; background-size: cover"><div class="caption"><h1>Používáme nejnovější webové <font color="#00ACAC">standardy</font></h1></div></li>
+    </ul>
+</section>
+
+<section id="about">
+<div class="container">
+    <h1 class="section-heading">Co děláme?</h1>
+    <div class="underline"></div>
+    <h3 class="section-heading">Děláme web. A děláme ho pořádně</h3>
+    <div class="row animated nonpacity">
+    <div id="test" class="col-md-4" style="text-align: center;">
+        <i class="fa fa-code fa-3x service"></i>
+        <h1>Webové prezentace</h1>
+        <p>Tvoříme firemní a produktové prezentace, bolgy, microsites dle nejnovějších standartů</p>
+    </div>
+    <div class="col-md-4" style="text-align: center;">
+        <i class="fa fa-cogs fa-3x service"></i>
+        <h1>Webové aplikace</h1>
+        <p>Vytváříme intranety, sociální sítě, mobilní aplikace a eshopy, které splňí všechny vaše potřeby. </p>
+    </div>
+    <div class="col-md-4" style="text-align: center; margin-bottom: 50px">
+        <i class="fa fa-wrench fa-3x service"></i>
+        <h1>Správa webu</h1>
+        <p>Web již máte, ale potřebujete jej průběžně spravovat a obměňovat? Nechte to na nás!</p>
+    </div>
+<div align="center"><a class="btn btn-primary">Dozvědět se více</a></div>
+    </div>
+       </div>
+    </section>
+    <section id="process" class="colored">
+	<div class="container">
+    <h2 class="section-heading">Jak to probíhá?</h2>
+    <div class="underline"></div>
+    <h3 class="section-heading">Tvoříme dle osvědčeného postu<strong></strong>pu, abychom se vždy dobrali nejlepšího výsledku.</h3>
+    <div class="animated nonpacity" align="center">
+    <img src="img/process.svg" width="100%" style="max-width: 700px;">
+    </div>
+    </div>
+</article>
+</section>
+<section id="team">
+<div class="container">
+    <h1 class="section-heading">Náš tým</h1>
+    <div class="underline"></div>
+    <h3 class="section-heading">Jsme tým mladých, kreativních a ambiciózních webdesignérů</h3>
+    <div class="row animated nonpacity">
+    <div class="col-md-4">
+    <a href="img/IMG_4870.jpg" data-lightbox="team" data-title="Filip Domes"><img src="img/IMG_4870.jpg" style="width: 250px; height: 300px"></a>
+    <h2>Filip Domes</h2>
+    <h3>Webdesignér</h3>
+    <p>domes@creacore.cz</p>
+    <p style="font-size:24px"><a href="#" class="fa fa-facebook" style="margin-right: 5px"></a><a href="#" class="fa fa-google-plus" style="margin-right: 5px"></a><a href="#" class="fa fa-twitter"></a></p>
+    </div>
+    
+    <div class="col-md-4">
+    <a href="img/IMG_4864.jpg" data-lightbox="team" data-title="Václav Brzezina"><img src="img/IMG_4864.jpg" style="width: 250px; height: 300px"></a>
+    <h2>Václav Brzezina</h2>
+    <h3>Webdesignér, programátor</h3>
+    <p>brzezina@creacore.cz</p>
+    <p style="font-size:24px"><a href="#" class="fa fa-facebook" style="margin-right: 5px"></a><a href="#" class="fa fa-google-plus" style="margin-right: 5px"></a><a href="#" class="fa fa-twitter"></a></p>
+    </div>
+    
+    <div class="col-md-4">
+    <a href="http://placehold.it/600x800" data-lightbox="team" data-title="Karolína penčáková"><img src="http://placehold.it/240x240"></a>
+    <h2>Karolína Penčáková</h2>
+    <h3>Copywriter, SEO, Programátor</h3>
+    <p>pencakova@creacore.cz</p>
+    <p style="font-size:24px"><a href="#" class="fa fa-facebook" style="margin-right: 5px"></a><a href="#" class="fa fa-google-plus" style="margin-right: 5px"></a><a href="#" class="fa fa-twitter"></a></p>
+    </div>
+    </div>
+    </div>
+</div>
+</section>
+<section id="skills" class="colored">
+<div class="container">
+<h2 class="section-heading">Co dokážeme?</h2>
+    <div class="underline"></div>
+<h3 class="section-heading">Jako tým dokážeme opravdu mnoho</h3>
+<div class="animated nonpacity">
+<div class="row">
+<div class="col-md-4 col-sm-6">
+<div class="skill">
+<div class="fa fa-user fa-2x visible animated"></div>
+<div class="fa fa-users fa-2x hid animated"></div>
+</div>
+<h3>User experience</h3>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+</div>
+<div class="col-md-4 col-sm-6">
+<div class="skill">
+<div class="fa fa-mobile fa-2x visible animated"></div>
+<div class="fa fa-desktop fa-2x hid animated"></div>
+</div>
+<h3>Responsive</h3>
+<p>Desktop, tablet, nebo telefon? Naše weby vypadají skvěle všude</p>
+</div>
+<div class="col-md-4 col-sm-6">
+<div class="skill">
+<div class="fa fa-camera fa-2x visible animated"></div>
+<div class="fa fa-photo fa-2x hid animated"></div>
+</div>
+<h3>Vlastní fotografie</h3>
+<p>Zajistíme vám vlastní profesionální fotografie pro všechny účely.</p>
+</div>
+<div class="col-md-4 col-sm-6">
+<div class="skill">
+<div class="fa fa-html5 fa-2x visible animated"></div>
+<div class="fa fa-css3 fa-2x hid animated"></div>
+</div>
+<h3>HTML5 &amp; CSS3</h3>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+</div>
+<div class="col-md-4 col-sm-6">
+<img src="http://placehold.it/100x100" style="border-radius: 50%; float: left; margin-right: 15px">
+<h3>Moderní responsivní design</h3>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+</div>
+<div class="col-md-4 col-sm-6">
+<img src="http://placehold.it/100x100" style="border-radius: 50%; float: left; margin-right: 15px">
+<h3>Něco dalšího</h3>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+</div>
+</div>
+</div>
+</section>
+<section id="work">
+<div class="container">
+<h1 class="section-heading">Ukázky naší práce</h1>
+    <div class="underline"></div>
+<h3 class="section-heading">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</h3>
+<div class="row animated nonpacity">
+<div class="col-md-3 col-sm-6 col-xs-12">
+<div class="thumb">
+<a><img src="http://placehold.it/260" width="100%"></a>
+<a class="fa fa-expand fa-3x thumb-expand animated" href="http://placehold.it/600x800" data-lightbox="work" data-title="My caption"></a>
+<a class="fa fa-eye fa-3x thumb-eye animated"></a>
+<div class="thumb-caption animated">Caption</div>
+</div>
+</div>
+<div class="col-md-3 col-sm-6 col-xs-12">
+<div class="thumb">
+<a><img src="http://placehold.it/260" width="100%"></a>
+<a class="fa fa-expand fa-3x thumb-expand animated" href="http://placehold.it/600x800" data-lightbox="work" data-title="My caption"></a>
+<a class="fa fa-eye fa-3x thumb-eye animated"></a>
+<div class="thumb-caption animated">Caption</div>
+</div>
+</div>
+<div class="col-md-3 col-sm-6 col-xs-12">
+<div class="thumb">
+<a><img src="http://placehold.it/260" width="100%"></a>
+<a class="fa fa-expand fa-3x thumb-expand animated" href="http://placehold.it/600x800" data-lightbox="work" data-title="My caption"></a>
+<a class="fa fa-eye fa-3x thumb-eye animated"></a>
+<div class="thumb-caption animated">Caption</div>
+</div>
+</div>
+<div class="col-md-3 col-sm-6 col-xs-12">
+<div class="thumb">
+<a><img src="http://placehold.it/260" width="100%"></a>
+<a class="fa fa-expand fa-3x thumb-expand animated" href="http://placehold.it/600x800" data-lightbox="work" data-title="My caption"></a>
+<a class="fa fa-eye fa-3x thumb-eye animated"></a>
+<div class="thumb-caption animated">Caption</div>
+</div>
+</div>
+
+<div align="center"><a class="btn btn-primary">Všechny reference</a></div>
+</div>
+</div>
+</div>
+</section>
+<section id="contact" class="colored">
+<div class="container">
+<h1 class="section-heading">Kontaktujte nás</h1>
+    <div class="underline"></div>
+<h3 class="section-heading" style="margin-bottom: 60px">Máte zájem o naše služby? Ozvěte se nám!</h3>
+<div class="row">
+<div class="col-md-12" style="text-align: center; padding-bottom: 30px">
+<div class="row">
+<div class="col-md-4 col-sm-4">
+<a target="_blank" href="https://www.google.cz/maps/place/Hlavn%C3%AD+1374,+739+11+Fr%C3%BDdlant+nad+Ostravic%C3%AD/@49.5869654,18.3592532,17z/data=!4m7!1m4!3m3!1s0x4713f59eef3658ab:0x2175ceac98b70fcc!2sHlavn%C3%AD+1374,+739+11+Fr%C3%BDdlant+nad+Ostravic%C3%AD!3b1!3m1!1s0x4713f59eef3658ab:0x2175ceac98b70fcc"><i class="fa fa-map-marker fa-5x"></i></a>
+<h2>Sídlo</h2>
+<h3>Hlavní 1374, Frýdlant nad Ostravicí</h3>
+</div>
+<div class="col-md-4 col-sm-4">
+<a href="mailto:info@creacore.cz"><i class="fa fa-envelope fa-5x"></i></a>
+<h2>Email</h2>
+<h3>info@creacore.cz</h3>
+</div>
+<div class="col-md-4 col-sm-4	">
+<a href="tel://+420 604 716 317"><i class="fa fa-phone fa-5x"></i></a>
+<h2>Telefon</h2>
+<h3>+420 604 716 317</h3>
+</div>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-md-8 col-md-offset-2" style="padding: 30px">
+<input class="form-control" type="text" placeholder="Jméno" style="margin-bottom: 15px">
+<input class="form-control" type="text" placeholder="Email" style="margin-bottom: 15px">
+<input class="form-control" type="text" placeholder="Předmět" style="margin-bottom: 15px">
+<textarea class="form-control" placeholder="Zpráva" style="margin-bottom: 15px"></textarea>
+<input class="btn btn-primary btn-block" type="submit" value="Odeslat">
+</div>
+</div>
+</div>
+</section>
+
+<footer class="container-fluid">
+    <p style="font-size:28px; margin-bottom: 15px"><a href="#" class="fa fa-facebook"></a><a href="#" class="fa fa-google-plus"></a><a href="#" class="fa fa-twitter"></a></p>
+    <p style="font-size: 18px;">&copy; Václav Brzezina 2015</p>
+  <a href="#top" id="up"><i class="fa fa-chevron-up fa-2x"></i></a>
+<script src="js/js.min.js"></script>
+    
+</footer>
+</body>
+</html>
